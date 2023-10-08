6d76811c68bb1bf7aac9f2056c71a46f49c6f169
Christian Kohlschütter <christian@kohlschutter.com>
2023-10-09 00:13:46 +0200

    util: Update JavaHomeLocator for standalone-home

--- standalone-util/src/main/java/com/kohlschutter/jdk/standaloneutil/JavaHomeLocator.java
+++ standalone-util/src/main/java/com/kohlschutter/jdk/standaloneutil/JavaHomeLocator.java
@@ -1,8 +1,8 @@
 package com.kohlschutter.jdk.standaloneutil;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.FileSystems;
@@ -15,8 +15,7 @@ import java.util.concurrent.ExecutionException;
  * Provides the path for "java.home", as far as the compiler is concerned.
  */
 public class JavaHomeLocator {
-  private static final Path COMPILER_JAVA_HOME = new File(
-      "/Library/Java/JavaVirtualMachines/jdk-11.0.20.1+1/Contents/Home").toPath();
+  private static final Path COMPILER_JAVA_HOME = pathForResourceURL();
 
   private static final CompletableFuture<FileSystem> JRT_FS = newCompilerJrtFS();
 
@@ -45,5 +44,53 @@ public class JavaHomeLocator {
 
   public static Path getCtSymPath() {
     return getCompilerJavaHome().resolve("lib").resolve("ct.sym");
+  }
+
+  private static Path pathForResourceURL() {
+    try {
+      return pathForResourceURL(Class.forName("com.kohlschutter.jdk.home.JavaHome"), ".");
+    } catch (ClassNotFoundException e) {
+      throw new IllegalStateException("Could not find standalone-home jar in classpath", e);
+    }
+  }
+
+  private static Path pathForResourceURL(Class<?> classRef, String name) {
+    boolean parentDir = ".".equals(name);
+
+    if (parentDir || name.isEmpty()) {
+      name = classRef.getSimpleName() + ".class";
+    }
+
+    URI uri;
+    try {
+      uri = classRef.getResource(name).toURI();
+    } catch (URISyntaxException e) {
+      throw new IllegalStateException(e);
+    }
+
+    Path p;
+    try {
+      p = Path.of(uri);
+    } catch (FileSystemNotFoundException e) {
+      String ssp = uri.getSchemeSpecificPart();
+      int exclp = ssp.indexOf("!/");
+      if (exclp == -1) {
+        throw e;
+      }
+
+      FileSystem fs;
+      try {
+        fs = FileSystems.newFileSystem(uri, Collections.emptyMap());
+      } catch (IOException e1) {
+        throw (FileSystemNotFoundException) new FileSystemNotFoundException().initCause(e1);
+      }
+      p = fs.getPath(ssp.substring(exclp + 1));
+    }
+
+    if (parentDir) {
+      p = p.getParent();
+    }
+
+    return p;
   }
 }
