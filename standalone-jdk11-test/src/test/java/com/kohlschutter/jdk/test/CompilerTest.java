124da24793479ed5b18c3dd19f41c4a9a2498b20
Christian Kohlschütter <christian@kohlschutter.com>
2023-10-13 20:04:43 +0200

    jdk11-test: More tests

--- standalone-jdk11-test/src/test/java/com/kohlschutter/jdk/test/CompilerTest.java
+++ standalone-jdk11-test/src/test/java/com/kohlschutter/jdk/test/CompilerTest.java
@@ -1,13 +1,17 @@
 package com.kohlschutter.jdk.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,8 +23,8 @@ public class CompilerTest {
     assertEquals("11", JavaCompiler.version().split("\\.")[0]);
   }
 
-  @Test
-  public void testCompileJava7() throws Exception {
+  private void testCompileSimple(String version, boolean release, int expectedRc)
+      throws IOException {
     Path tmpDir = Files.createTempDirectory("standaloneJavacTest");
     Path helloJava = tmpDir.resolve("Hello.java");
     Path helloClass = tmpDir.resolve("Hello.class");
@@ -36,17 +40,53 @@ public class CompilerTest {
 
       System.out.println(tmpDir);
 
-      int rc = standalone.com.sun.tools.javac.Main.compile(new String[] {
-          "-source", "1.7", //
-          "-target", "1.7", //
-          helloJava.toAbsolutePath().toString()});
-      assertEquals(0, rc);
+      List<String> args = new ArrayList<>();
+      if (version != null) {
+        if (release) {
+          args.addAll(List.of("--release", version));
+        } else {
+          args.addAll(List.of("-source", version, "-target", version));
+        }
+      }
+      args.add(helloJava.toAbsolutePath().toString());
 
-      assertTrue(Files.exists(helloClass));
+      int rc = standalone.com.sun.tools.javac.Main.compile(args.toArray(new String[0]));
+      assertEquals(expectedRc, rc);
+
+      if (rc == 0) {
+        assertTrue(Files.exists(helloClass));
+      } else {
+        assertFalse(Files.exists(helloClass));
+      }
     } finally {
       Files.deleteIfExists(helloClass);
       Files.deleteIfExists(helloJava);
       Files.delete(tmpDir);
     }
+  }
+
+  @Test
+  public void testCompileDefault() throws Exception {
+    testCompileSimple(null, false, 0);
+  }
+
+  @Test
+  public void testCompileJava7() throws Exception {
+    testCompileSimple("1.7", false, 0);
+  }
+
+  @Test
+  public void testCompileJava8() throws Exception {
+    testCompileSimple("1.8", false, 0);
+  }
+
+  @Test
+  public void testCompileJava11() throws Exception {
+    testCompileSimple("11", true, 0);
+  }
+
+  @Test
+  public void testCompileJava12() throws Exception {
+    testCompileSimple("12", true, 2); // not supported
   }
 }
